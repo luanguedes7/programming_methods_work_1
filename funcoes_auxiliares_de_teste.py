1f7d3e99a2833c4c033c8579a1b9bad8139a2b39
luanguedes7 <lcrisley98@gmail.com>
2023-01-08 21:54:55 -0300

    Teste 2- Verificação de existência de somente 8 rainhas; Implementação 2 (funcoes_auxiliares)

--- funcoes_auxiliares_de_teste.py
+++ funcoes_auxiliares_de_teste.py
@@ -1,12 +1,15 @@
+'''Alguma funções destinadas para o módulo de testes'''
+
 def verifica_se_ordem_da_matrix_e_8(matrix):
     '''Verifica se a ordem de uma dada matrix é 8'''
-    
+
     contador_linhas = 0
     contador_colunas = 0
 
     for i in range(len(matrix)):
             contador_linhas =+ 1
-            for j in range(i):
+
+            for j in range(len(matrix[i])):
                 contador_colunas += 1
 
                 if contador_colunas != 8:
@@ -20,3 +23,14 @@ def verifica_se_ordem_da_matrix_e_8(matrix):
         return 8
     else:
         return 0
+
+def verifica_se_ha_8_rainhas(matrix):
+
+    contador_de_rainhas = 0
+
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            if matrix[i][j] == 1:
+                contador_de_rainhas += 1
+    
+    return contador_de_rainhas
--- testa_oito_rainhas.py
+++ testa_oito_rainhas.py
@@ -1,6 +1,6 @@
 ''' Importações de funções'''
 from oito_rainhas import tabuleiro
-from funcoes_auxiliares_de_teste import verifica_se_ordem_da_matrix_e_8
+from funcoes_auxiliares_de_teste import *
 
 def teste_validade_tabuleiro():
     '''Verifica se o tabuleiro dado pelo usuário é de ordem 8'''
@@ -10,3 +10,10 @@ def teste_validade_tabuleiro():
     ordem_tabuleiro = verifica_se_ordem_da_matrix_e_8(tabuleiro)
 
     assert ordem_tab == ordem_tabuleiro
+
+def teste_de_verificacao_de_8_rainhas():
+    '''Verifica se o tabuleiro dado pelo usuário apresenta somente 8 rainhas'''
+
+    num_rainhas = verifica_se_ha_8_rainhas(tabuleiro)
+
+    assert num_rainhas == 8
